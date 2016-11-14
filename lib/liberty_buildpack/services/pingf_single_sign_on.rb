fed43913b52a60f5761160633fadf360299d2db5
Yaro Dunchych <yaro@yaros-mbp.leoguest.fg.rbc.com>
2016-11-14 15:20:16 -0500

    testing

--- lib/liberty_buildpack/services/pingf_single_sign_on.rb
+++ lib/liberty_buildpack/services/pingf_single_sign_on.rb
@@ -278,17 +278,17 @@ module LibertyBuildpack::Services
       wasec = REXML::Element.new('webAppSecurity', doc.root)
       wasec.add_attribute('ssoRequireSSL', 'true')
       #YD 11142016 begin - enable app security by default
-      puts '-----> Creating security binding for authenticated-->ALL_AUTHENTICATED_USERS'
+      #puts '-----> Creating security binding for authenticated-->ALL_AUTHENTICATED_USERS'
       app = REXML::Element.new('webApplication',doc.root)
-      app.attributes['name'] = 'myapp'
-      app.attributes['location'] = 'myapp.war'
-      app.attributes['type'] = 'war'
+      app.add_attribute('name','myapp')
+      app.add_attribute('location','myapp.war')
+      app.add_attribute('type','war')
       app-bnd = app.add_element('application-bnd')
       security-role = app-bnd.add_element('security-role')
-      security-role.attributes['id'] = 'authenticated'
-      security-role.attributes['name'] = 'authenticated'
+      security-role.add_attribute('id','authenticated')
+      security-role.add_attribute('name', 'authenticated')
       special-subject = security-role.add_element('special-subject')
-      special-subject.attributes['type'] = 'ALL_AUTHENTICATED_USERS'
+      special-subject.add_attribute('type', 'ALL_AUTHENTICATED_USERS')
       #YD 11142016 end
     end
 
